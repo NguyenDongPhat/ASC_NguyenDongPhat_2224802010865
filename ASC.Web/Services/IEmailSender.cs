07257cc7c3ab2161e2b6aaa4d624e0dd91a26694
NguyenDongPhat <phatnd04@gmail.com>
2025-02-26 19:17:37 +0700

    Add project files.

---
+++ ASC.Web/Services/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASC.Solution.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
+
---
+++ ASC.Web/Services/ISmsSender.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASC.Solution.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
+
